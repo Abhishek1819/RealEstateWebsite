3c3c95109ead795451fb51c0ba78d5839e6d408d
Abhishek <abhishekgiri370@gmail.com>
2022-09-03 09:59:01 +0530

    my first commit reactwebsite

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,31 @@
-import React from 'react';
+import React from "react";
+
+// import link
+import { Link } from "react-router-dom";
+// import logo
+import Logo from "../assets/img/logo.png";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header className="py-6 mb-12 border-b">
+      <div className="container mx-[70px] flex justify-between items-center">
+        <Link to="/">
+          <img src={Logo} alt="" />
+        </Link>
+        <div className="flex items-center gap-6">
+          <Link className="hover:text-violet-900 transition" to="/">
+            Log in
+          </Link>
+          <Link
+            className="bg-green-600 hover:bg-violet-800 text-white px-4 py-3 rounded-lg transition"
+            to="/"
+          >
+            Sign up
+          </Link>
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import HouseContextProvider from "./components/HouseContext";
+import { BrowserRouter as Router } from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Router>
+    <HouseContextProvider>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </HouseContextProvider>
+  </Router>
 );
